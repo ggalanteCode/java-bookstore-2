176fb559245eb55a5e212c0dd59de3e73f4fc908
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-04 20:20:13 +0200

    calcolo sconto in percentuale libro

--- src/com/soprasteria/javabookstore/Libro.java
+++ src/com/soprasteria/javabookstore/Libro.java
@@ -6,6 +6,18 @@ public class Libro {
 		String titolo, editore, lingua, isbn;
 		int numPagine;
 		double peso, prezzo;
+		
+		prezzo = 34.20;
+		
+		double sconto = 20;
+		
+		double prezzoDaSottrarre = (prezzo/100)*sconto;
+		
+		double prezzoScontato = prezzo - prezzoDaSottrarre;
+		
+		System.out.println("Il prezzo iniziale del libro è: " + prezzo);
+		System.out.println("Lo sconto è del " + sconto + "%");
+		System.out.println("Il prezzo scontato del libro è: " + prezzoScontato);
 	}
 
 }
