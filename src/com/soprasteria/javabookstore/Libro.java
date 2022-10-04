7faa032914457535b745f16cf786de3e61bbbf32
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-04 20:17:54 +0200

    aggiunte variabili libro

--- src/com/soprasteria/javabookstore/Libro.java
+++ src/com/soprasteria/javabookstore/Libro.java
@@ -3,8 +3,9 @@ package com.soprasteria.javabookstore;
 public class Libro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String titolo, editore, lingua, isbn;
+		int numPagine;
+		double peso, prezzo;
 	}
 
 }
